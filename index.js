90c1326fe6ae8e259a88555a89d4ac28700e7c4e
Developsigner <35513194+SuhoBae@users.noreply.github.com>
2019-01-26 02:51:32 +0900

    190126_back to top button update
    190126_back to top button update

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-import styles from './styles.css';
+const topBtn = document.querySelector(".back-to-top");
+    SCROLLED_CN = "scrolled";
+    FADED_CN = "faded";
+
+
+function handleScroll () {
+    const scrollY = window.scrollY;
+    console.log(window.scrollY)
+    if(scrollY > 1100){
+        topBtn.classList.add(SCROLLED_CN);        
+        console.log("Back to Top");
+    }else{
+        topBtn.classList.remove(SCROLLED_CN);  
+        console.log("disappear");
+    }
+}
+
+
+
+window.addEventListener("scroll",handleScroll);
